f7b8422aa63a240c11e96e1d2f76d90d03711d4f
Mateo <samirsb2306@gmail.com>
2025-05-27 21:53:35 -0500

    Cambios a addEvenListener

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ document.getElementById('addTaskBtn').addEventListener('click', () => {
     const li = document.createElement('li');
     li.textContent = taskText;
     li.addEventListener('click', () => li.classList.toggle('done'));
-    li.addEventListener('dblclick', () => li.remove());
-
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Eliminar';
+    deleteBtn.addEventListener('click', () => li.remove());
+    li.appendChild(deleteBtn);
+    
     document.getElementById('taskList').appendChild(li);
     input.value = '';
 });
